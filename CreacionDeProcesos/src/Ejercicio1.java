1489c10d5b1a433e3e402abff1d6f5e166c1370c
adria <adria@DESKTOP-6KLG4EG>
2020-11-18 11:23:43 +0100

    Ejercicio 1 al 3

--- CreacionDeProcesos/src/Ejercicio1.java
+++ CreacionDeProcesos/src/Ejercicio1.java
@@ -1,4 +1,17 @@
 
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Ejercicio1 {
 
+	public static void main(String[] args) {
+		Runtime rt = Runtime.getRuntime();
+		Process p = null;
+		try {
+			p = rt.exec("firefox");
+			InputStreamReader leer = new InputStreamReader(null);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
